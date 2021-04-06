72106d8d42ea0fcfb538ad5efa527f2a031b29e3
LackOfDream <galaxy0419@gmail.com>
2021-04-06 23:19:15 +0900

    add pod yaml generator

---
+++ gen_pod.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cat <<EOF
+apiVersion: v1
+kind: Pod
+metadata:
+  name: yahagi-go
+spec:
+  containers:
+  - image: ${YUUBARIGO_IMAGE:-yuubarigo}:$(git rev-parse HEAD)
+    args:
+    - -debug
+    - -interval
+    - "2"
+    - -retry
+    - "10"
+    - -kcp
+    - http://127.0.0.1:8081
+    - -expedNotify
+    - -tgToken
+    - "${TGTOKEN}"
+    - -tgUser
+    - "${TGUSER}"
+    name: yuubarigo
+    ports:
+    - name: yuubarigo
+      containerPort: 8099
+      protocol: TCP
+  - image: ${KCP_IMAGE:-kccacheproxy}:v2.6.3
+    name: kccacheproxy
+    ports:
+    - name: kcp
+      containerPort: 8081
+      protocol: TCP
+    volumeMounts:
+    - mountPath: /app/cache
+      name: kcp
+  hostNetwork: true
+  volumes:
+  - name: kcp
+    hostPath:
+      path: /regi/kcp
+      type: Directory
+EOF
